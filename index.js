45cca9d9e4996165b3de473984165a9bb2d3fd16
robocon321 <robocon321n@gmail.com>
2019-11-21 22:40:37 +0700

    Add pug into project and write urls for project

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-var express=require('express');
-var app= express();
-var port =3000;
+var express = require('express');
+var app = express();
+var port = 3000;
 
-app.get('',function(req,res){
-	res.send("<h1>Hello world</h1>");
+app.set("view engine", 'pug');
+app.set("views", "./views");
+app.get('', function(req, res) {
+    res.send("<h1>Hello world</h1><a href='header'>Header</a><a href='/users'>User</a>");
 });
 
-app.get('/users',function(req,res){
-	res.send("This is user list");
+app.get('/header', function(req, res) {
+    res.render('index', {
+        name: "Robocon321#"
+    });
 });
 
-app.listen(port,function(){
-	console.log("My port have just been opened success...");
+app.get('/users', function(req, res) {
+    res.render('users/index.pug', {
+        names: [
+            { id: 1, name: "Wing" },
+            { id: 2, name: "Hong 10" },
+            { id: 3, name: "Victor" },
+            { id: 4, name: "Skim" }
+        ]
+    })
+});
+
+app.listen(port, function() {
+    console.log("My port have just been opened success...");
 });
