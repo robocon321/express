46cef72a79476baaf36ac607783ad75e98e0f91b
robocon321 <robocon321n@gmail.com>
2019-11-22 16:49:30 +0700

    Add file view.pug

--- index.js
+++ index.js
@@ -2,64 +2,28 @@ var express = require('express');
 var app = express();
 var port = 3000;
 var bodyParser=require('body-parser');
-
-var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
-
-var adapter = new FileSync('db.json')
-var db = low(adapter)
-
-var shortid=require("shortid");
-
-db.defaults({users:[]}).write();
-var users=db.get('users').value();
+var userRoute=require("./routes/users.js");
+var db=require("./db.js");
+var controllers=require("./controllers/controllers.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/users',userRoute);
 
 app.set("view engine", 'pug');
 app.set("views", "./views");
-app.get('', function(req, res) {
+
+app.get('/', function(req, res) {
     res.send("<h1>Hello world</h1><a href='header'>Header</a><br/><a href='/users'>User</a>");
 });
 
-app.get('/header', function(req, res) {
-    res.render('index', {
-        name: "Robocon321#"
-    });
-});
-
-app.get('/users', function(req, res) {
-    res.render('users/index.pug', {
-        users: users
-    })
-});
-
-app.get('/users/search',function(req,res){
-	var q=req.query.q;
-	var userMatchs=users.filter(function(user){
-		return user.name.toLowerCase().indexOf(q.toLowerCase())>=0;
-	});
-	res.render('users/index.pug',{
-		users:userMatchs
-	});
-});
-
-app.get('/users/create',function(req,res){
-	res.render('users/create');
-});
-
-app.post('/users/create',function(req,res){
-	db.get('users').push({id:shortid.generate(),name:req.body.name}).write();
-	users=db.get("users").value();
-	res.redirect('/users');
-})
+app.get('/header', controllers.header);
 
 app.get('/view/:id',function(req,res){
 	var id=req.params.id;
 	var user=db.get('users').find({id:id}).value();
 	console.log(user);
-	res.render("../view",{user:user});
+	res.render("./view",{user:user});
 })
 
 app.listen(port, function() {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db=require("../db.js");
+var users=db.get('users').value();
+var controllers=require('../controllers/controllers.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', controllers.index);
+
+router.get('/search',controllers.search);
+
+router.get('/create',controllers.create_get);
+
+router.post('/create',controllers.create_post);
+
 
 module.exports = router;
