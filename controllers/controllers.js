758bbbae3421838653fd08d1b1fa9e4c8913aaaf
robocon321 <robocon321n@gmail.com>
2019-11-23 08:16:44 +0700

    Validate for project

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -20,8 +20,24 @@ module.exports.search=function(req,res){
 module.exports.create_get=function(req,res){
 	res.render('users/create');
 };
+
 module.exports.create_post=function(req,res){
-	db.get('users').push({id:shortid.generate(),name:req.body.name}).write();
-	users=db.get("users").value();
-	res.redirect('/');
+	var errors=[];
+	var values=[req.body.name,req.body.phone];
+	if(req.body.name==""){
+		errors.push("Name is empty");
+	}
+	if(req.body.phone==""){
+		errors.push("Phone is empty");
+	}
+	if(errors.length){
+		console.log(values);
+		res.render("users/create",{
+			errors:errors,
+			values:values
+		});
+		return ;
+	}
+	db.get('users').push({id:shortid.generate(),name:req.body.name,phone:req.body.phone}).write();
+	res.redirect('/users');
 };
