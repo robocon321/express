98f235178428f6f08605b20b555a11af7b591201
robocon321 <robocon321n@gmail.com>
2019-11-24 13:29:45 +0700

    Modify  file product.js

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var product=require('../controllers/product.js');
+var login =require('../controllers/login.js');
 /* GET home page. */
-router.get('/',product.index);
+router.get('/',login.getLogin, product.index);
 
 module.exports = router;
