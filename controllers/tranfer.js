ffffbfc3f5caffe046744882607fb225cea3c6ff
robocon321 <robocon321n@gmail.com>
2019-12-03 09:30:32 +0700

    Add CSRF to protect

--- controllers/tranfer.js
+++ controllers/tranfer.js
@@ -1,11 +1,14 @@
  var db=require('../db.js');
 module.exports.postData=function(req,res,next){
  	var userReceive=req.body.user;
- 	var amount=req.body.amount;
+ 	var amount=parseInt(req.body.amount);
  	var userSend=req.signedCookies.id;
- 	db.get('tranfer').push({userSend:userSend,amount:amount,userSend:userSend}).write();
+ 	console.log(userReceive,amount,userSend);
+ 	db.get('tranfers').push({userSend:userSend,amount:amount,userReceive:userReceive}).write();
  	res.redirect('/tranfer');
 }
 module.exports.getData=function(req,res,next){
-	res.render('../views/users/tranfer.pug');
+	res.render('../views/users/tranfer.pug',{
+		csrfToken: req.csrfToken()
+	});
 }
--- routes/tranfer.js
+++ routes/tranfer.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var login=require('../controllers/login.js');
 var tranfer=require("../controllers/tranfer.js");
+var csrf=require('csurf');
+router.use(csrf({ cookie: true }));
 
 router.post('/', login.getLogin, tranfer.postData);
 router.get('', login.getLogin, tranfer.getData);
